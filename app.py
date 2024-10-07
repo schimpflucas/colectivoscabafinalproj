e822edd9421b821a7697812df3a7b2ebdf385e48
schimpflucas <schimpflucas@hotmail.com>
2024-10-07 12:25:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from fend_pagina1 import mostrar_pagina1
 from fend_pagina2 import mostrar_pagina2
 
-@st.cache_data(ttl=300)
 
 st.sidebar.title("Navegación")
 pagina = st.sidebar.selectbox("Selecciona una página", ("Mapa de Consulta", "Estadisticas"))
